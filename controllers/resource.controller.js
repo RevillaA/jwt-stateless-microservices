38e637fd69ddfd01a8aa50bff5cc6c9088ca89d6
RevillaA <revillaantonio212005@hotmail.com>
2026-06-20 21:44:56 -0500

    refactor: separar errores logicos de operacionales

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -1,9 +1,6 @@
 export class ResourceController {
     static getAlphaPrivateData(req, res) {
-        return res.status(200).json({
-            message: 'Acceso autorizado al microservicio Alpha.',
-            user: req.user
-        });
+        throw new Error('Conexion perdida con la BDD');
     }
 
     static getBetaPrivateData(req, res) {
